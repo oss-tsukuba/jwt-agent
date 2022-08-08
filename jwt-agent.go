9888c4c9ddd151810b3b9f6b14729d7a0689e34b
Atsushi Kumazaki <kuma@canaly.co.jp>
2022-08-08 14:11:55 +0900

    get token and update it regularly

--- jwt-agent.go
+++ jwt-agent.go
@@ -1,115 +1,200 @@
 package main
 
 import (
-	"bufio"
-	"flag"
-	"fmt"
-	"io"
-	"net/http"
-	"net/url"
-	"os"
-	"strconv"
-	"strings"
+  "bufio"
+  "flag"
+  "fmt"
+  "io"
+  "log"
+  "log/syslog"
+  "net/http"
+  "net/url"
+  "os"
+  "strconv"
+  "strings"
+  "time"
+  "encoding/json"
+  "os/user"
+
+  b64 "encoding/base64"
+  
+  "golang.org/x/crypto/ssh/terminal"
 )
 
 var (
-	server     = flag.String("server", "can3.canaly.co.jp", "JWT Server")
-	lock       = flag.String("lock", "/tmp/jwt-agent.pid", "Process ID")
-	userId     string
-	passphrase string
+  server     = flag.String("s", "", "JWT Server")
+  lock       = flag.String("lock", "/tmp/jwt-agent.pid", "Process ID")
+  userId     = flag.String("l", "", "User Name")
+  uid        string
+  passphrase string
 )
 
 func init() {
-	var oldPid string
+  var oldPid string
 
-	fp, err := os.Open(*lock)
-	if err == nil {
-		defer fp.Close()
-		scanner := bufio.NewScanner(fp)
-		for scanner.Scan() {
-			oldPid = scanner.Text()
-		}
+  logger, err := syslog.New(syslog.LOG_INFO, "jwt-agent")
+  if err != nil {
+    panic(err)
+  }
+  log.SetOutput(logger)
 
-		fmt.Printf("old pid:%s\n", oldPid)
-		targetPid, err := strconv.Atoi(oldPid)
-		process, err := os.FindProcess(targetPid)
-		if err == nil {
-			err = process.Kill()
-			if err != nil {
-				panic(err)
-			}
-		}
-	}
+  cuser, err := user.Current()
+  if err != nil {
+     log.Fatalln(err)
+     panic(err)
+  }
 
-	pid := os.Getpid()
+  uid = cuser.Uid
+  
+	
+  fp, err := os.Open(*lock)
+  if err == nil {
+    defer fp.Close()
+    scanner := bufio.NewScanner(fp)
+    for scanner.Scan() {
+      oldPid = scanner.Text()
+    }
 
-	file, err := os.Create(*lock)
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
+    targetPid, err := strconv.Atoi(oldPid)
+    process, err := os.FindProcess(targetPid)
+    if err == nil {
+      err = process.Kill()
+      if err != nil {
+         log.Println(err)
+      }
+    }
+  }
 
-	_, err = file.WriteString(strconv.Itoa(pid))
-	if err != nil {
-		panic(err)
-	}
+  pid := os.Getpid()
+
+  file, err := os.Create(*lock)
+  if err != nil {
+    log.Fatalln(err)  
+    panic(err)
+  }
+  defer file.Close()
+
+  _, err = file.WriteString(strconv.Itoa(pid))
+  if err != nil {
+    log.Fatalln(err)
+    panic(err)
+  }
 }
 
 func getToken(userId string, passphrase string) (string, error) {
-	endpoint := fmt.Sprintf("https://%s/jwt-server/jwt", *server)
-	fmt.Println(endpoint)
+  endpoint := fmt.Sprintf("https://%s/jwt-server/jwt", *server)
 
-	values := url.Values{}
-	values.Set("user", userId)
-	values.Add("pass", passphrase)
+  values := url.Values{}
+  values.Set("user", userId)
+  values.Add("pass", passphrase)
 
-	req, err := http.NewRequest(
-		"POST",
-		endpoint,
-		strings.NewReader(values.Encode()),
-	)
-	if err != nil {
-		return "", err
-	}
+  req, err := http.NewRequest(
+    "POST",
+    endpoint,
+    strings.NewReader(values.Encode()),
+  )
+  if err != nil {
+    return "", err
+  }
 
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+  req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
+  client := &http.Client{}
+  resp, err := client.Do(req)
+  if err != nil {
+    return "", err
+  }
+  defer resp.Body.Close()
 
-	body, _ := io.ReadAll(resp.Body)
-	token := string(body)
+  if resp.StatusCode != 200 {
+    return "", fmt.Errorf("bad response status code %d", resp.StatusCode)
+  }
 
-	return token, nil
+  body, _ := io.ReadAll(resp.Body)
+  token := string(body)
+
+  if token == "" {
+    return "", fmt.Errorf("authentication error")
+  }
+
+  filename := "/tmp/jwt_user_u" + uid
+  file, err := os.Create(filename)
+  if err != nil {
+      return "", err
+  }
+  defer file.Close()
+  _, err = file.WriteString(token)
+  if err != nil {
+      return "", err
+  }
+
+  log.Println("get token...")
+  
+  return token, nil
 }
 
-func updateToken(userId string, passphrase string) (string, error) {
-	fmt.Println("updateToken")
+func parseToken(tokenString string) (int64, error) {
+  header := strings.Split (tokenString,".")
+  str_payload := strings.Replace(header[1],"-","+",-1)
+  str_sig := strings.Replace(str_payload,"_","/",-1)
 
-	return "", nil
+  llx := len(str_sig)
+  nnx := ((4 - llx % 4) % 4)
+  ssx := strings.Repeat("=" , nnx)
+  str := strings.Join([]string{str_sig, ssx}, "")
+  bytes, err :=  b64.StdEncoding.DecodeString(str)
+  if err != nil {
+    return 0, err
+  }
+
+  uEnc := b64.URLEncoding.EncodeToString([]byte(bytes))
+  decode, _ := b64.URLEncoding.DecodeString(uEnc)
+
+  var decode_data interface{}
+  
+  if err = json.Unmarshal(decode, &decode_data); err != nil {
+   return 0, err
+  }
+
+  data := decode_data.(map[string]interface{})
+  exp := data["exp"].(float64)
+  
+  now := time.Now().Unix()
+
+  limit := (exp - float64(now)) * 0.8
+
+  return int64(limit), nil
 }
 
 func main() {
-	flag.Parse()
+  flag.Parse()
 
-	fmt.Print("userId:")
-	fmt.Scan(&userId)
+  if (*server == "" || *userId == "") {
+    fmt.Println("Usage: jwt-agent -s {server} -l {user}")
+    return
+  }
 
-	fmt.Print("passphrase:")
-	fmt.Scan(&passphrase)
+  fmt.Print("passphrase:")
+  passphrase, err := terminal.ReadPassword(0)
+  if err != nil {
+    log.Fatalln(err)
+    panic(err)
+  }
+  fmt.Println()
 
-	fmt.Printf("%s:%s\n", userId, passphrase)
+  for {
+    token, err := getToken(*userId, string(passphrase))
+    if err != nil {
+      log.Fatalln(err)
+      panic(err)
+    }
 
-	token, err := getToken(userId, passphrase)
+    limit, err := parseToken(token)
+    if err != nil {
+      log.Fatalln(err)
+      panic(err)
+    }
 
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(token)
-
+    time.Sleep(time.Duration(limit) * time.Second)  
+  }
 }
